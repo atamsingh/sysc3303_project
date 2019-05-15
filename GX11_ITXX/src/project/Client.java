830f54e7ae65437e5af175330a24575277d8d0b5
Atamjeet Singh <atamjeet.singh@shopify.com>
2019-05-15 10:41:48 -0400

    adding more comments

--- GX11_ITXX/src/project/Client.java
+++ GX11_ITXX/src/project/Client.java
@@ -22,12 +22,15 @@ public class Client {
 	
 	public void main() {
 		Client c = new Client();
-		// get input 
+		// one or more Java threads
+		// get input (filename, write/read) -- normal vs. test, verbose vs. quiet, IP address of server, client/server directory, shut down, etc.
+		//  Reduce the amount of input required each time by having options to change values, rather than having to input them each time
 		// ask all the questions that needs to be asked 
 		// call the appropriate functions to kick WRD or RRQ correctly
 		
 		// if exit asked for... gracefully close all the requests and exit. < this means we need to track when write and read requesta are going 
 		// luckily this is not threaded just 1 main thread per client.
+		// after completed - client should permit the user to initiate another file transfer.
 		c.writeRequest("", null);
 		c.readRequest("", null);
 	}
--- GX11_ITXX/src/project/Server.java
+++ GX11_ITXX/src/project/Server.java
@@ -25,12 +25,21 @@ public class Server {
 	public void startListening() {
 		// listen to the port needed for requests 
 		// create threads to handle these req. received
+		// One thread will wait on port 69 for UDP datagrams (that should contain RRQ or WRQ packets)
+		// create another thread (call it the client connection thread), and pass it the TFTP packet to deal with
 		this.handleRequest(); // send data along
+		// go back to waiting on port 69 for another request. 
 	}
 
 	public void main() {
 		// get ready to accept 
+		//  supporting multiple concurrent read and write connections with different clients
+		// For each RRQ, the server should respond with DATA block 1 and 0 bytes of data (no file I/O). For each WRQ the server should respond with ACK block 0. 
 		Server s = new Server();
 		s.startListening();
+		
+		// elegant shutdown
+		// finish all file transfers,  but refuse to create new connections with clients
+		
 	}
 }
