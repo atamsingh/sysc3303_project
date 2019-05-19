11601685af7f6d6500270422b360de0a0209c80f
Atamjeet Singh <atamjeet.singh@shopify.com>
2019-05-19 01:12:15 -0400

    added tests for commons

--- GX11_ITXX/src/test/TestClient.java
+++ GX11_ITXX/src/test/TestClient.java
@@ -1,16 +1,35 @@
 package test;
+
+import java.net.UnknownHostException;
+import java.util.function.Function;
+
 public class TestClient {
+	DummyInputLoader inputLoader = null;
+	project.Client client = null;
 	
-	project.Client client = new project.Client();
-	
-	private boolean test_creation() {
-		return true;
-	}
-	
-	public void run() throws Exception {
-		// call tests
-		if(!this.test_creation()) {
+	Function<Void, Boolean> test_creation = (Void v) -> {return client != null;};
+
+	public void handleTest(String func_name, Function<Void, Boolean> func) throws Exception {
+		System.out.println("running test `TestClient`.`" + func_name + "`...");
+		Boolean test_return = func.apply(null);
+		if(!test_return) {
 			throw new Exception();
 		}
 	}
+
+	public void run() throws Exception {
+		inputLoader = new DummyInputLoader();
+		client = new project.Client(inputLoader);
+		
+		// call tests
+		handleTest("test_creation", test_creation);
+	}
+}
+
+class DummyInputLoader extends project.ClientInputLoader {
+
+	public DummyInputLoader() throws UnknownHostException {
+		super();
+	}
+	
 }
--- GX11_ITXX/src/test/TestCoordinator.java
+++ GX11_ITXX/src/test/TestCoordinator.java
@@ -3,13 +3,20 @@ package test;
 public class TestCoordinator {
 	
 	public static void main(String[] args) {
+		System.out.println("Running tests....");
+		
 		try {
 			TestClient client_tester = new TestClient();
 			client_tester.run();
+			TestCommon common_tester = new TestCommon();
+			common_tester.run();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Tests Failed.");
 			System.exit(1);
 		}
+
+		System.out.println("All tests successfully parsed....");
 	}
 }
+
