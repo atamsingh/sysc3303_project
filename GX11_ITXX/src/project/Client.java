a05c4c5cfb647cf7a038ea2b5434abdbf05c766f
Atamjeet Singh <atamjeet.singh@shopify.com>
2019-05-28 00:34:08 -0400

    Add Retransmit on Timeout on Client Write + Correct ACK Client Read

--- GX11_ITXX/src/project/Client.java
+++ GX11_ITXX/src/project/Client.java
@@ -3,13 +3,23 @@ package project;
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Client {
 	
 	Commons common = null;
 	ClientInputLoader input_grabber = null;
 	private DatagramSocket sendReceiveSocket;
+	final Duration timeout = Duration.ofSeconds(1);
+	final int max_retry = 10;
 	
 	public Client(ClientInputLoader input) {
 		common = new Commons("CLIENT");
@@ -21,6 +31,64 @@ public class Client {
 		}
 	}
 	
+	private DatagramPacket tryReceivingUntilSuccessful(DatagramSocket s, DatagramPacket p, int block_to_be_received, int size) {
+		// send request and wait on response
+		// we keep waiting until we hear something. Server should retransmit if something not ack'ed
+		DatagramPacket response = common.sendRequestAndWaitOnResponse(s, p, size);
+		// we received a response, lets make sure it's not null and is the block we except (the one after the last one)
+		while(response == null || Commons.getBlockNumber(response) != block_to_be_received) {
+			// while this condition is not met, keep waiting.
+			response = common.receiveRequest(s, size);
+		}
+		// once we have a response for the next block we expected, return
+		return response;
+	}
+	
+	private DatagramPacket trySendingUntilSuccessful(DatagramSocket s, DatagramPacket p, int block_num) {
+		return trySendingUntilSuccessful(s, p, block_num, 1);
+	}
+	
+	private DatagramPacket trySendingUntilSuccessful(DatagramSocket s, DatagramPacket p, int block_num, int counter) {
+		// if request counter higher than max try return null
+		if(counter > max_retry) {
+			return null;
+		}
+		// else create executor
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+
+		// create future promise for request
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		final Future<DatagramPacket> handler = executor.submit(new Callable() {
+		    public DatagramPacket call() throws Exception {
+		        return common.sendRequestAndWaitOnResponse(s, p);
+		    }
+		});
+		
+		// try to call this
+		try {
+			this.logVerbose("making request to server and waiting (iteration: " + counter + ")...");
+			// wait for timeout to get response. 
+			DatagramPacket response = handler.get(timeout.toMillis(), TimeUnit.MILLISECONDS);
+			// if no exception, confirm the acknowledgement is correct.
+			if(common.confirmAcknowledgement(response, block_num)) {
+				// if the ack received is correct, send response back/
+				this.logVerbose("Response received and acknowledged.");
+				executor.shutdownNow();
+				return response;
+			}else {
+				// if ack received is not for the block we anticipated, try sending again and only return when successful or max retry limit hit
+				this.logVerbose("Response received but is not for the block that needed to be acknowledged... sending again...");
+				executor.shutdownNow();
+				return trySendingUntilSuccessful(s, p, block_num, counter++);
+			}
+		} catch (TimeoutException | InterruptedException | ExecutionException e) {
+			// if timeout happens, or some other error occurs. clean everything and try sending again.
+			handler.cancel(true);
+			executor.shutdownNow();
+		    return trySendingUntilSuccessful(s, p, block_num, counter++);
+		}
+	}
+
 	public void logVerbose(String message) {
 		if(this.input_grabber.mode.contentEquals("verbose")) {
 			common.print(message);
@@ -77,18 +145,25 @@ public class Client {
 	private void readRequest(String file_name_to_read, String file_name_to_write_to) throws IOException {
 		// TODO - Clean this up. Yuck!!
 		// generate read request header
+		
 		byte[] data = this.generatePacket(file_name_to_read, 1);
 		DatagramPacket readRequestPacket = new DatagramPacket(data, data.length, input_grabber.server_address, input_grabber.server_port);
 		this.logVerbose("Sending RRQ packet to server on " + input_grabber.server_address + ":" + input_grabber.server_port + ".");
-		DatagramPacket responseFromServer = common.sendRequestAndWaitOnResponse(this.sendReceiveSocket,readRequestPacket, 516);
+		DatagramPacket responseFromServer  = null;
 		int blocks_received = 1;
+		while(responseFromServer == null) {
+			responseFromServer = this.tryReceivingUntilSuccessful(this.sendReceiveSocket, readRequestPacket, blocks_received, 516);
+		}
 		this.logVerbose("received packet " + blocks_received + "... ");
 		byte[] curr_response = common.filterPackage(responseFromServer);
 		byte[] all_data = curr_response;
 		while(curr_response.length >= 512) {
 			byte[] ack_data = common.generateAcknowledgement(blocks_received);
 			DatagramPacket curr_ack = new DatagramPacket(ack_data, ack_data.length, responseFromServer.getAddress(), responseFromServer.getPort());
-			responseFromServer = common.sendRequestAndWaitOnResponse(this.sendReceiveSocket,curr_ack, 1024);
+			responseFromServer = null;
+			while(responseFromServer == null) {
+				responseFromServer = this.tryReceivingUntilSuccessful(this.sendReceiveSocket, curr_ack, blocks_received, 1024);
+			}
 			blocks_received++;
 			this.logVerbose("received packet " + blocks_received + "... ");
 			curr_response = common.filterPackage(responseFromServer);
@@ -111,6 +186,7 @@ public class Client {
 		}
 	}
 	
+	
 	private void sendFileToServer(String file_name_to_read, InetAddress address, int port) {
 		this.logVerbose("Reading " + file_name_to_read + " from client disk.");
 		byte[] file_data = this.getFileData(file_name_to_read);
@@ -124,10 +200,9 @@ public class Client {
 			byte[] data_to_send = common.generateDataPacket(curr_data, curr_block_number);
 			this.logVerbose("sending block " + curr_block_number + " from byte index " + start_block_index + " to " + end_block_index + ".");
 			DatagramPacket dataPacket = new DatagramPacket(data_to_send, data_to_send.length, address, port);
-			DatagramPacket responseFromServer = common.sendRequestAndWaitOnResponse(this.sendReceiveSocket, dataPacket);
-			if(!common.confirmAcknowledgement(responseFromServer)) {
-				this.logQuiet("Error sending data to server. Confirm that the server is still available and listening.");
-				break;
+			DatagramPacket responseFromServer = null; 
+			while(responseFromServer == null) {
+				responseFromServer = this.trySendingUntilSuccessful(this.sendReceiveSocket, dataPacket, curr_block_number); // also confirms correct block ack or keeps trying
 			}
 			this.logVerbose("Successfully received acknowledgement for block " + curr_block_number + ".");
 		}
@@ -135,23 +210,22 @@ public class Client {
 		this.logQuiet("File " + file_name_to_read + " sent over to server at " + address + ":" + port + ".");
 	}
 	
+	
 	private void writeRequest(String file_name_to_read, String file_name_to_write_to) throws UnknownHostException {
 		// generate write request header
 		this.logVerbose("Generating WRQ packet for write request");
 		byte[] data = this.generatePacket(file_name_to_write_to, 2);
 		DatagramPacket writeRequestPacket = new DatagramPacket(data, data.length, input_grabber.server_address, input_grabber.server_port);
 		this.logVerbose("Sent request to server for wrq.");
-		DatagramPacket responseFromServer = common.sendRequestAndWaitOnResponse(this.sendReceiveSocket, writeRequestPacket);
-		// confirm it is acknowledged
-		if(common.confirmAcknowledgement(responseFromServer)) {
-			this.logVerbose("Received Acknowledgement from the server. confirming if actually acknowledged.");
-			this.logVerbose("starting to send file to server.");
-			// start sending file
-			this.sendFileToServer(file_name_to_read, responseFromServer.getAddress(), responseFromServer.getPort());
-		}else {
-			this.logVerbose("Response from server received but not acknowledged.");
-			this.logQuiet("Error sending data to server. Confirm that the server is still available and listening.");
+		DatagramPacket responseFromServer = null;
+		while(responseFromServer == null) {
+			this.logVerbose("Tring to send ack...");
+			responseFromServer = this.trySendingUntilSuccessful(this.sendReceiveSocket, writeRequestPacket, 0); // block 0 for ack
 		}
+		this.logVerbose("Received Acknowledgement from the server.");
+		this.logVerbose("starting to send file to server.");
+		// start sending file
+		this.sendFileToServer(file_name_to_read, responseFromServer.getAddress(), responseFromServer.getPort());
 	}
 
 	private void handleRequest() throws IOException {
@@ -182,6 +256,7 @@ public class Client {
 		this.logQuiet("Goodbye!");
 	}
 
+	
 	public static void main(String[] args) {
 		try {
 			ClientInputLoader cig;
